4c8de864d9a9d5bea82973956103ed08019d038b
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-02-18 17:30:39 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { TripsModule } from './trips/trips.module';
 import { StatsModule } from './stats/stats.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Trip } from './trips/trips.entity';
-import { Connection } from 'typeorm';
 import { DatabaseConfig } from './database/database.config';
 
 @Module({
