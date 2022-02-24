6d4b3dee4781f43eb4ebb1768cd7a880da2aea0c
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-02-24 13:48:53 +0100

    should return an array of monthly stats objects

--- src/stats/controller/stats.controller.spec.ts
+++ src/stats/controller/stats.controller.spec.ts
@@ -12,6 +12,22 @@ describe('StatsController', () => {
         total_price: '0PLN',
       };
     }),
+    getMonthlyStats: jest.fn(() => {
+      return [
+        {
+          date: 'February, 16th',
+          total_distance: '3521km',
+          avg_ride: '97.82km',
+          avg_price: '60.46PLN',
+        },
+        {
+          date: 'February, 17th',
+          total_distance: '0km',
+          avg_ride: '0.00km',
+          avg_price: '68.00PLN',
+        },
+      ];
+    }),
   };
 
   beforeEach(async () => {
@@ -38,5 +54,21 @@ describe('StatsController', () => {
       total_distance: expect.stringMatching(kmRegex),
       total_price: expect.stringMatching(PLNRegex),
     });
+  });
+
+  it('should return an array of monthly stats objects', async () => {
+    const kmRegex = /([0-9]+)(km)/gm;
+    const PLNRegex = /([0-9]+)(PLN)/gm;
+
+    expect(await controller.getMonthlyStats()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          date: expect.any(String),
+          total_distance: expect.stringMatching(kmRegex),
+          avg_ride: expect.stringMatching(kmRegex),
+          avg_price: expect.stringMatching(PLNRegex),
+        }),
+      ]),
+    );
   });
 });
