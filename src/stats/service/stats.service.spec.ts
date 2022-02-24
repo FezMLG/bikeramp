dc5e3e62f70b5654e19d4b6e1c1b760416a4cfe7
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-02-24 13:53:04 +0100

    should return an object of weekly stats

--- src/stats/service/stats.service.spec.ts
+++ src/stats/service/stats.service.spec.ts
@@ -4,7 +4,14 @@ import { StatsService } from './stats.service';
 describe('StatsService', () => {
   let service: StatsService;
 
-  const mockTripsRepository = {};
+  const mockTripsRepository = {
+    getWeeklyStats: jest.fn(() => {
+      return {
+        total_distance: '0km',
+        total_price: '0PLN',
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,4 +27,30 @@ describe('StatsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return an object of weekly stats', async () => {
+    const kmRegex = /([0-9]+)(km)/gm;
+    const PLNRegex = /([0-9]+)(PLN)/gm;
+
+    expect(await service.getWeeklyStats()).toEqual({
+      total_distance: expect.stringMatching(kmRegex),
+      total_price: expect.stringMatching(PLNRegex),
+    });
+  });
+
+  // it('should return an array of monthly stats objects', async () => {
+  //   const kmRegex = /([0-9]+)(km)/gm;
+  //   const PLNRegex = /([0-9]+)(PLN)/gm;
+
+  //   expect(await service.getMonthlyStats()).toEqual(
+  //     expect.arrayContaining([
+  //       expect.objectContaining({
+  //         date: expect.any(String),
+  //         total_distance: expect.stringMatching(kmRegex),
+  //         avg_ride: expect.stringMatching(kmRegex),
+  //         avg_price: expect.stringMatching(PLNRegex),
+  //       }),
+  //     ]),
+  //   );
+  // });
 });
