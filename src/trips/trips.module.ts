f24e68ca0d314308f01963f310616340865820e7
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-02-24 17:15:53 +0100

    updating paths

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TripsController } from './controller/trips.controller';
 import { TripsService } from './service/trips.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Trip } from 'src/schema/trip.entity';
+import { Trip } from '../schema/trip.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Trip])],
